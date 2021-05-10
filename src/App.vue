65b8b5e364168147a27416a51105f54ec38e7d2e
182Marco <marcomilza@gmail.com>
2021-05-10 11:49:28 +0200

    inizializzato con sass

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+    },
+  };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  /* fonts */
+  @import '~@fontsource/montserrat/index.css';
+  @import '~@fontsource/montserrat/700.css';
+  /* parcials */
+  @import '@/scss/var';
+  @import '@/scss/reset';
+  @import '@/scss/mixins';
+
+  #app {
+    margin: 0;
+    padding: 0;
+    background-color: black;
+    height: 100vh;
+  }
 </style>
